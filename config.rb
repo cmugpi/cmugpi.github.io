0e1cf7280e6028683ec98139f1caee0281222348
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-11-22 21:52:54 -0500

    Initialized standalone Compass project

---
+++ config.rb
@@ -0,0 +1,21 @@
+require 'sass-globbing'
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/~15131/"
+http_images_path = "images"
+http_generated_images_path = "images"
+http_fonts_path = "fonts"
+css_dir = "css"
+
+# Local development paths
+sass_dir = "_sass"
+images_dir = "images"
+fonts_dir = "fonts"
+
+# TODO turn this back off
+line_comments = true
+# output_style = :compressed
