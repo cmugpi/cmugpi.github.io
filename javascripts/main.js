ab7448d2801868f55d71b6d56d06e093809d57f9
Jacob Zimmerman <zimmerman.jake@gmail.com>
2015-01-12 22:04:53 -0500

    Fixed changes introduced by updating Semantic UI

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,7 +12,7 @@
   function fixTOC() {
     // Create ui vertical menu, fill it with old links,
     // remove li tags, and add .item to a tags
-    var $toc = $('<div class="ui fixed vertical menu"></div>');
+    var $toc = $('<div class="ui vertical menu"></div>');
     $toc.html($('#markdown-toc').html());
     $toc.find('a').unwrap().addClass('item');
     $('#markdown-toc').replaceWith($toc);
